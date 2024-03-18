95502f4184afc69077645e27d4d153ce0a5b0b1a
Hao Lam <tuonghao2001@gmail.com>
2024-03-18 09:26:45 -0700

    RSpec4

--- RSpec4/lib/part_1.rb
+++ RSpec4/lib/part_1.rb
@@ -1,39 +1,19 @@
+def my_reject(arr, &blk)
+    arr.select { |el| !blk[el] }
+end
 
+def my_one?(arr, &blk)
+    arr.count { |el| blk[el] } == 1
+end
 
+def hash_select(hash, &blk)
+    hash.reject { |k, v| !blk[k, v] }
+end
 
+def xor_select(arr, prc1, prc2)
+    arr.select { |el| (prc1[el] || prc2[el]) && !(prc1[el] && prc2[el]) }
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def my_reject(arr, &blk)
-#     arr.select { |el| !blk[el] }
-# end
-
-# def my_one?(arr, &blk)
-#     arr.count { |el| blk[el] } == 1
-# end
-
-# def hash_select(hash, &blk)
-#     hash.reject { |k, v| !blk[k, v] }
-# end
-
-# def xor_select(arr, prc1, prc2)
-#     arr.select { |el| (prc1[el] || prc2[el]) && !(prc1[el] && prc2[el]) }
-# end
-
-# def proc_count(val, arr)
-#     arr.count { |prc| prc[val] }
-# end
+def proc_count(val, arr)
+    arr.count { |prc| prc[val] }
+end
--- RSpec4/lib/part_2.rb
+++ RSpec4/lib/part_2.rb
@@ -1,44 +1,24 @@
+def proper_factors(n)
+    (1...n).select { |i| n % i == 0 }
+end
 
+def aliquot_sum(n)
+    proper_factors(n).sum
+end
 
+def perfect_number?(n)
+    return false if aliquot_sum(n) != n
+    true
+end
 
+def ideal_numbers(n)
+    ideal_nums = []
+    num = 5     # smallest perfect number is 6
 
+    while ideal_nums.length != n
+        num += 1
+        ideal_nums << num if perfect_number?(num)
+    end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def proper_factors(n)
-#     (1...n).select { |i| n % i == 0 }
-# end 
-
-# def aliquot_sum(n)
-#     proper_factors(n).sum
-# end
-
-# def perfect_number?(n)
-#     return false if aliquot_sum(n) != n
-#     true
-# end
-
-# def ideal_numbers(n)
-#     ideal_nums = []
-#     num = 5     # smallest perfect number is 6
-
-#     while ideal_nums.length != n
-#         num += 1
-#         ideal_nums << num if perfect_number?(num)
-#     end
-
-#     ideal_nums
-# end
+    ideal_nums
+end
