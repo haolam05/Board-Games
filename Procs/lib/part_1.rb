208d48c91b3a5b32329161e5b311ad8df9dbb38d
Hao Lam <tuonghao2001@gmail.com>
2024-03-18 09:32:56 -0700

    Procs

--- Procs/lib/part_1.rb
+++ Procs/lib/part_1.rb
@@ -1,3 +1,35 @@
+def my_map(arr, &prc)
+    new_arr = []
+    arr.each { |ele| new_arr << prc[ele] }
+    new_arr
+end
+
+def my_select(arr, &prc)
+    new_arr = []
+    arr.each { |ele| new_arr << ele if prc[ele] }
+    new_arr
+end
+
+def my_count(arr,&prc)
+    count = 0
+    arr.each { |ele| count += 1 if prc[ele] }
+    count
+end
+
+def my_any?(arr, &prc)
+    arr.each { |ele| return true if prc[ele] }
+    false
+end
+
+def my_all?(arr, &prc)
+    arr.each { |ele| return false  if !prc[ele] }
+    true
+end
+
+def my_none?(arr, &prc)
+    arr.each { |ele| return false if prc[ele] }
+    true
+end
 
 
 
@@ -17,92 +49,39 @@
 
 
 
+def my_map(arr, &block)
+    arr.map(&block)
+end
 
+def my_map(arr, &block)
+    new_arr = []
+    arr.each { |ele| new_arr << block.call(ele) }
+    new_arr
+end
 
-# def my_map(arr, &prc)
-#     new_arr = []
-#     arr.each { |ele| new_arr << prc[ele] }
-#     new_arr
-# end
+def my_select(arr, &block)
+    new_arr = []
+    arr.each {|ele| new_arr << ele if block.call(ele) }
+    new_arr
+end
 
-# def my_select(arr, &prc)
-#     new_arr = []
-#     arr.each { |ele| new_arr << ele if prc[ele] }
-#     new_arr
-# end
+def my_count(arr, &block)
+    count = 0
+    arr.each { |ele| count += 1 if block.call(ele) }
+    count
+end
 
-# def my_count(arr,&prc)
-#     count = 0
-#     arr.each { |ele| count += 1 if prc[ele] }
-#     count
-# end
+def my_any?(arr, &block)
+    arr.each { |ele| return true if block.call(ele) }
+    false
+end
 
-# def my_any?(arr, &prc)
-#     arr.each { |ele| return true if prc[ele] }
-#     false
-# end
+def my_all?(arr, &block)
+    arr.each { |ele| return false if !block.call(ele) }
+    true
+end
 
-# def my_all?(arr, &prc)
-#     arr.each { |ele| return false  if !prc[ele] }
-#     true
-# end
-
-# def my_none?(arr, &prc)
-#     arr.each { |ele| return false if prc[ele] }
-#     true
-# end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def my_map(arr, &block)
-#     arr.map(&block)
-# end
-
-# def my_map(arr, &block)
-#     new_arr = []
-#     arr.each { |ele| new_arr << block.call(ele) }
-#     new_arr
-# end
-
-# def my_select(arr, &block)
-#     new_arr = []
-#     arr.each {|ele| new_arr << ele if block.call(ele) }
-#     new_arr
-# end
-
-# def my_count(arr, &block)
-#     count = 0
-#     arr.each { |ele| count += 1 if block.call(ele) }
-#     count
-# end
-
-# def my_any?(arr, &block)
-#     arr.each { |ele| return true if block.call(ele) }
-#     false
-# end
-
-# def my_all?(arr, &block)
-#     arr.each { |ele| return false if !block.call(ele) }
-#     true
-# end
-
-# def my_none?(arr, &block)
-#     arr.each { |ele| return false if block.call(ele) }
-#     true
-# end
+def my_none?(arr, &block)
+    arr.each { |ele| return false if block.call(ele) }
+    true
+end
--- Procs/lib/part_2.rb
+++ Procs/lib/part_2.rb
@@ -1,47 +1,26 @@
+def reverser(str, &prc)
+    prc[str.reverse]
+end
 
+def word_changer(sentence, &prc)
+    sentence.split.map { |ele| prc[ele] }.join(' ')
+end
 
+def greater_proc_value(num, prc1, prc2)
+    return prc1[num] if prc1[num] > prc2[num]
+    prc2[num]
+end
 
+def and_selector(arr, prc1, prc2)
+    arr.select { |ele| prc1[ele] && prc2[ele] }
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def reverser(str, &prc)
-#     prc[str.reverse]
-# end
-
-# def word_changer(sentence, &prc)
-#     sentence.split.map { |ele| prc[ele] }.join(' ')
-# end
-
-# def greater_proc_value(num, prc1, prc2)
-#     return prc1[num] if prc1[num] > prc2[num]
-#     prc2[num]
-# end
-
-# def and_selector(arr, prc1, prc2)
-#     arr.select { |ele| prc1[ele] && prc2[ele] }
-# end
-
-# def alternating_mapper(arr, prc1, prc2)
-#     arr.map.with_index do |ele, i|
-#         if i.even?
-#             prc1[ele]
-#         else
-#             prc2[ele]
-#         end
-#     end
-# end
+def alternating_mapper(arr, prc1, prc2)
+    arr.map.with_index do |ele, i|
+        if i.even?
+            prc1[ele]
+        else
+            prc2[ele]
+        end
+    end
+end
